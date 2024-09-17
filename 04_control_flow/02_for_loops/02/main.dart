91804475d39918d99a33493a35b3e65b88c22265
Fardan Nurhidayat <129834353+Fardan-Nurhidayat@users.noreply.github.com>
2024-09-17 15:23:50 +0700

    Update main.dart

--- 04_control_flow/02_for_loops/02/main.dart
+++ 04_control_flow/02_for_loops/02/main.dart
@@ -1,3 +1,6 @@
 void main() {
-  // tulis kodemu di sini.
+  // saya menggunakan print dengan * yang dikali setiap * mungkin ini bukan best pratices 
+  for (int i = 1; i <= 10; i++) {
+   print("*" * i);
+  }
 }
